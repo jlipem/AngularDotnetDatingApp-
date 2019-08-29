b10aaa5bd5936b1f9b980ca067a43120efab4a32
Josh Lipe-Melton <jlipem@gmail.com>
2019-08-29 10:08:45 -0500

    added login and registration to front end

---
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {};
+
+  constructor(
+    private authService: AuthService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    this.authService.register(this.model).subscribe(() => {
+      console.log('register success!');
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  cancel() {
+    console.log(this.model);
+    this.cancelRegister.emit(false);
+  }
+
+}
